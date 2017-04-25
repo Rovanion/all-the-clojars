9447391b14dcd97b1c6e5b772082500175654033
Rovanion Luckey <rovanion.luckey@gmail.com>
2017-04-25 10:58:57 +0200

    Corrected lein-deps script, didn't actualy fetch all deps.

--- lein-deps-all-fake-projects.bash
+++ lein-deps-all-fake-projects.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
+base_dir=$(dirname $(realpath $0))
 
-for folder in fake-projects/*; do
+echo $base_dir
+
+for project_file in $(find fake-projects/ -iname project.clj); do
+    folder=$(dirname ${project_file})
     echo Working in $folder
     cd $folder
     lein deps
-    cd ../..
+    cd $base_dir
 done
